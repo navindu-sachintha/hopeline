21f43002a774f669b38f03f2a73bc1312e97399f
navindu-sachintha <navindu.social@gmail.com>
2024-10-12 21:33:09 +0530

    Added sanity for CM

--- src/sanity/env.ts
+++ src/sanity/env.ts
@@ -1,5 +1,5 @@
 export const apiVersion =
-  process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2024-10-12'
+  process.env.NEXT_PUBLIC_SANITY_API_VERSION ?? '2024-10-12'
 
 export const dataset = assertValue(
   process.env.NEXT_PUBLIC_SANITY_DATASET,
