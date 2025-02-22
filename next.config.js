37be5da6f910331835f278b2b673539c64385507
navindu-sachintha <navindu.social@gmail.com>
2025-02-22 20:50:31 +0530

    Revert "✨ Integrate Sentry for error monitoring and performance tracking, add example API route and client-side error handling"
    This reverts commit dd8026c52bc96658758db46e9c08e7a2b6b32a69.

---
+++ next.config.js
@@ -0,0 +1,29 @@
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+
+/** @type {import("next").NextConfig} */
+const config = {
+  images:{
+    remotePatterns:[
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io'
+      },
+      {
+        protocol: 'https',
+        hostname: 'aqurocm.blob.core.windows.net',
+      },
+    ],
+  },
+  experimental:{
+    serverComponentsExternalPackages: ['tesseract.js'],
+    outputFileTracingIncludes: {
+      '/api/**/*': ['node_modules/.pnpm/tesseract.js-core@5.1.1/node_modules/tesseract.js-core/tesseract-core-simd.wasm']
+    }
+  }
+};
+
+export default config;
